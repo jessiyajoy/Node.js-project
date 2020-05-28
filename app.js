709cf47e2fbdead2d626bc684361209e3f2143a0
Jessiya Joy <jessiyajoy@Jessiyas-MacBook-Pro.local>
2020-05-28 14:08:04 +0530

    Adds base files

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path')
+    //const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+// app.engine(
+//     'hbs',
+//     expressHbs({
+//         layoutsDir: 'views/layouts/',
+//         defaultLayout: 'main-layout',
+//         extname: 'hbs'
+//     }));
+// app.set('view engine', 'hbs');
+//app.set('view engine', 'pug');
+
+app.set('views', 'views')
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.router);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' })
+})
+
+app.listen(3000);
