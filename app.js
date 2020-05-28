eb22b75ba5273dc2ff1709569283b3a4120201a4
Jessiya Joy <jessiyajoy@Jessiyas-MacBook-Pro.local>
2020-05-28 16:59:03 +0530

    Deleting .pug and .handlebars files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
     //const expressHbs = require('express-handlebars');
 
 const app = express();
+const errorController = require('./controllers/error');
 
 app.set('view engine', 'ejs');
 // app.engine(
@@ -18,17 +19,15 @@ app.set('view engine', 'ejs');
 
 app.set('views', 'views')
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.router);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' })
-})
+app.use(errorController.get404page)
 
 app.listen(3000);
